d9f11d982b55248ef06cb306010be9008a025695
Mrsalt56 <mrsalt56@gmail.com>
2025-08-18 17:43:09 -0700

    r

---
+++ games/ItalianBrainrotClicker/StartPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace ItalianBrainrotClicker.Pages;
+
+public partial class StartPage : ContentPage
+{
+    public StartPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnStartClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(GamePage));
+    }
+}
