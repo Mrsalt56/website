57060f252fec5f9833ef97cd1c4765093f3ff6d0
Mrsalt56 <mrsalt56@gmail.com>
2025-09-13 19:34:40 -0700

    d

--- games/Celari/src/config.ts
+++
@@ -1,8 +0,0 @@
-// Used in both the SW and client
-
-//@ts-ignore
-const { __celari__$config } = self;
-
-const $config = __celari__$config; // just rename it
-
-export default $config;
--- games/Celari/static/sw.js
+++
@@ -1,6 +0,0 @@
-importScripts("celari.config.js");
-importScripts("sw.bundle.js");
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(self.routeCelari(event.request));
-});
